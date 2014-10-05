b7fef9701e0a33779273dc01825742954a14cd5c
Charlie Taveras <charlie@flatiron.com>
2014-10-05 13:08:28 -0400

    Add flymake-cursor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,13 @@
 (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/python-mode.el-6.1.3/")
+(setq py-install-directory "~/.emacs.d/python-mode.el-6.1.3/")
+(require 'python-mode)
 (load "google-c-style.el")
+
 (setq line-number-mode t)
 (setq column-number-mode t)
 (setq-default show-trailing-whitespace t)
+
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.mm\\'" . objc-mode))
 (define-key global-map (kbd "M-g") 'goto-line)
@@ -18,5 +23,12 @@
 (add-hook 'c-mode-common-hook (lambda() (local-set-key (kbd "C-c o") 'ff-find-other-file)))
 (add-hook 'c-mode-common-hook (lambda() (c-set-offset 'substatement-open 0)))
 (add-hook 'c-mode-common-hook 'google-set-c-style)
-
+(setq sgml-basic-offset 2)
+(setq js-indent-level 2)
 (setq ff-search-directories '("." "../src" "../include" "~/work/client"))
+(setq-default indent-tabs-mode nil)
+
+
+(package-initialize)
+(elpy-enable)
+(require 'flymake-cursor)
